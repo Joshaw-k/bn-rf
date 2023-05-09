bd6a5a5214924d666ea4520ebaf15148299a6894
awinrin emefiele-konyeri <awinrin4bless@yahoo.com>
2023-05-09 08:14:35 +0100

    made the site mobile friendly

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,11 @@ import {BsFillArrowDownCircleFill} from "react-icons/bs"
 
 const Hero = () => {
   return (
-      <div className='container m-auto p-20'>
-        <img src={BlocknodeRectification} alt="hero.png" className='sm:w-full md:w-1/2 m-auto' />
-      <div className='w-90 m-auto text-white text-center p-10'>
+      <div className='container mx-auto p-8 my-10'>
+        <div className='sm:w-full mx-auto mb-5'>
+          <img src={BlocknodeRectification} alt="hero.png" className='w-full' />
+        </div>
+      <div className='w-95 m-auto text-white text-center p-2'>
         <p className='mb-8'>Every digital artwork on Upside is authentic and truly unique.</p>
 
         <p className='mb-8'>Blockchain technology makes this new approach to digital ownership possible.<br/>
@@ -17,9 +19,9 @@ const Hero = () => {
 
         <p className='mb-8'>You will be on a chat with an Artificial Intelligence Robot with zero Human interference.</p>
       </div>
-      <div className='flex gap-x-5 w-[37%] m-auto'>
-        <button className='py-3 px-8 bg-[rgba(0,201,32,1)] rounded-full font-bold '>Connect Wallet</button>
-        <button className='pl-5 flex-1 bg-transparent rounded-full font-bold inline-flex gap-x-14 text-[rgba(0,201,32,1)] ring-1 ring-inset ring-[rgba(0,201,32,1)] justify-center items-center' >Select Chain <BsFillArrowDownCircleFill className='text-4xl bg-white rounded-full'/></button>
+      <div className='flex justify-between m-auto'>
+        <button className='py-3 px-4 bg-[rgba(0,201,32,1)] rounded-full font-bold hover:text-[rgba(0,201,32,1)] hover:bg-white'>Connect Wallet</button>
+        <button className='pl-5 pr-1 bg-transparent rounded-full font-bold inline-flex gap-x-14 text-[rgba(0,201,32,1)] ring-1 ring-inset ring-[rgba(0,201,32,1)] justify-center items-center' >Select Chain <BsFillArrowDownCircleFill className='text-4xl bg-white rounded-full'/></button>
       </div>
       </div>
   )
