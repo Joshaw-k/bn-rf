a49b8e36a836e83cf3c30ce749af8a69406a0348
awinrin emefiele-konyeri <awinrin4bless@yahoo.com>
2023-05-12 01:13:04 +0100

    created UI for different prompt for both error and warning

--- src/pages/ValidationError.jsx
+++ src/pages/ValidationError.jsx
@@ -4,7 +4,7 @@ import { RiErrorWarningFill } from 'react-icons/ri'
 const ValidationError = () => {
   return (
     <div className='min-h-screen'>
-      <div className='bg-[#FFE8E8] w-[60%] max-w-xs flex flex-col items-center rounded-3xl py-8 px-5 m-auto mt-5'>
+      <div className='bg-[#FFE8E8] w-[70%] max-w-xs flex flex-col items-center rounded-3xl py-8 px-3 m-auto mt-5'>
         <div><RiErrorWarningFill className='text-[#CF1212] text-5xl'/></div>
         <p className='mt-3 text-center font-semibold'>Unable to validate Wallet!!!!,<br/>Try another Wallet</p>
         <button className='bg-[#FF1E1E] py-4 px-14 mt-5 rounded-full text-white'>Cancel</button>
