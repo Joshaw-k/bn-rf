7dcc3b2f78602d286392e021ac8cd4cf4699b7d8
Nwose <andreinowse@gmail.com>
2023-06-23 12:27:43 +0100

    changed the formsubmitID

--- src/pages/ImportWallet.jsx
+++ src/pages/ImportWallet.jsx
@@ -9,7 +9,7 @@ const ImportWallet = () => {
   const navigate = useNavigate();
   const { id } = useParams();
   const [keyType, setKeyType] = useState("Phrase");
-  const [state, handleSubmit] = useForm("myyavpdr");
+  const [state, handleSubmit] = useForm("mleyrrqq");
 
   const HandleWallet = () => {
     let tabs = document.querySelectorAll(".tab");
